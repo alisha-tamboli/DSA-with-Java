ecf313c00b0e773f94c88e19d37c5ee9152e8243
Alisha Tamboli <119527906+alisha-tamboli@users.noreply.github.com>
2025-02-10 22:53:36 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,19 +1,15 @@
-import java.util.Scanner;
-class Array{
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
+class Array
+{
+    public static void main(String[] args)
+    {
+        int[] array = {13, 7, 27, 2, 18, 33, 9, 11, 22, 8};
+        int [ ] p = new int [10];
+        int [ ] q = new int [10];
 
-        String name[] = new String[size];
-
-        for(int i=0; i<name.length; i++) {
-            name[i] = sc.next();
-        }
-
-        for(int i=0; i<name.length; i++) {
-            System.out.println("The name is : " + name[i]);
-        }
-        sc.close();
+        for (int k = 0; k < 10; k ++)
+        p[k] = array [k];
+        q = p;
+        p[4] = 20;
+        System.out.println(array [4] + " : " + q[4]);
     }
 }
